9efd5d2df9ddba7581487891daf57707e8171398
St Michael Egenamba <michaelegenamba@gmail.com>
2026-06-01 18:42:20 -0700

    Finished filters.py

--- lib/filters.py
+++ lib/filters.py
@@ -7,4 +7,6 @@ def filter_students_by_major(student_list, major):
     - Check if a student's major matches the given major (case insensitive).
     - Return a new list containing only students that match.
     """
-    pass
+    filtered_students = [student for student in student_list if major.lower() == student[2].lower()]
+    return filtered_students
+    
